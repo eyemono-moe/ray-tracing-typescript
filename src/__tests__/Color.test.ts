64065fcc05d0e231a9f824e4ba771eb6d4458eba
detteiu8383 <ysdr83@gmail.com>
2022-08-16 13:59:22 +0900

    Add test

---
+++ src/__tests__/Color.test.ts
@@ -0,0 +1,28 @@
+import Color from "../Color";
+
+describe("Color", () => {
+    test("constructor", () => {
+        const c = new Color(1, 2, 3);
+        expect(c.e[0]).toBe(1);
+        expect(c.e[1]).toBe(2);
+        expect(c.e[2]).toBe(3);
+    })
+
+    test("legalize", () => {
+        const c = new Color(1, -2, 3);
+        const c2 = Color.legalize(c);
+        expect(c2.e[0]).toBe(1);
+        expect(c2.e[1]).toBe(0);
+        expect(c2.e[2]).toBe(1);
+    })
+
+    test("writeColor", () => {
+        const c = new Color(1, 0, 0.5);
+        const buf = new Uint8Array(4);
+        Color.writeColor(c, 0, buf);
+        expect(buf[0]).toBe(255);
+        expect(buf[1]).toBe(0);
+        expect(buf[2]).toBe(127);
+        expect(buf[3]).toBe(255);
+    })
+})
---
+++ src/__tests__/Vector.test.ts
@@ -0,0 +1,94 @@
+import Vector from "../Vector"
+
+describe("Vector", () => {
+  test("constructor", () => {
+    const v = new Vector(1, 2, 3)
+    expect(v.e[0]).toBe(1)
+    expect(v.e[1]).toBe(2)
+    expect(v.e[2]).toBe(3)
+  })
+
+  test("add", () => {
+    const v1 = new Vector(1, 2, 3)
+    const v2 = new Vector(2, 3, 4)
+    const v3 = Vector.add(v1, v2)
+    expect(v3.e[0]).toBe(3)
+    expect(v3.e[1]).toBe(5)
+    expect(v3.e[2]).toBe(7)
+  })
+
+  test("sub", () => {
+    const v1 = new Vector(1, 2, 3)
+    const v2 = new Vector(2, 3, 4)
+    const v3 = Vector.sub(v1, v2)
+    expect(v3.e[0]).toBe(-1)
+    expect(v3.e[1]).toBe(-1)
+    expect(v3.e[2]).toBe(-1)
+  })
+
+  test("scale", () => {
+    const v1 = new Vector(1, 2, 3)
+    const v2 = Vector.scale(v1, 2)
+    expect(v2.e[0]).toBe(2)
+    expect(v2.e[1]).toBe(4)
+    expect(v2.e[2]).toBe(6)
+  })
+
+  test("mul", () => {
+    const v1 = new Vector(1, 2, 3)
+    const v2 = new Vector(2, 3, 4)
+    const v3 = Vector.mul(v1, v2)
+    expect(v3.e[0]).toBe(2)
+    expect(v3.e[1]).toBe(6)
+    expect(v3.e[2]).toBe(12)
+  })
+
+  test("div", () => {
+    const v1 = new Vector(2, 4, 6)
+    const v2 = Vector.div(v1, 2)
+    expect(v2.e[0]).toBe(1)
+    expect(v2.e[1]).toBe(2)
+    expect(v2.e[2]).toBe(3)
+  })
+
+  test("mag", () => {
+    const v1 = new Vector(1, 2, 3)
+    expect(Vector.mag(v1)).toBeCloseTo(Math.sqrt(14))
+  })
+
+  test("lengthSquared", () => {
+    const v1 = new Vector(1, 2, 3)
+    expect(Vector.lengthSquared(v1)).toBe(14)
+  })
+
+  test("dot", () => {
+    const v1 = new Vector(1, 2, 3)
+    const v2 = new Vector(2, 3, 4)
+    expect(Vector.dot(v1, v2)).toBe(20)
+  })
+
+  test("cross", () => {
+    const v1 = new Vector(1, 2, 3)
+    const v2 = new Vector(2, 3, 4)
+    const v3 = Vector.cross(v1, v2)
+    expect(v3.e[0]).toBe(-1)
+    expect(v3.e[1]).toBe(2)
+    expect(v3.e[2]).toBe(-1)
+  })
+
+  test("normalize", () => {
+    const v1 = new Vector(1, 2, 3)
+    const v2 = Vector.normalize(v1)
+    expect(v2.e[0]).toBeCloseTo(1 / Math.sqrt(14))
+    expect(v2.e[1]).toBeCloseTo(2 / Math.sqrt(14))
+    expect(v2.e[2]).toBeCloseTo(3 / Math.sqrt(14))
+  })
+
+  test("normalize zero vector", () => {
+    const v1 = new Vector(0, 0, 0)
+    const v2 = Vector.normalize(v1)
+    expect(v2.e[0]).toBe(0)
+    expect(v2.e[1]).toBe(0)
+    expect(v2.e[2]).toBe(0)
+  })
+})
